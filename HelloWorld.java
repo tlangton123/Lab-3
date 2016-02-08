e07d50b2759891d0c3fa47c97eac81f439ed68f1
Zach <z-ch@users.noreply.github.com>
2016-02-08 17:35:41 -0500

    Update HelloWorld.java
    Should compile now.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,9 @@
-public static void main(args[] string){
-System.out.print("Hello World");
-System.out.print("codecutr was here!");
+// This should compile now!  
+// Mon Feb  8 17:34:21 EST 2016
+// Zach, Cat, Alyssa, Tim, Sean
+public class HelloWorld {
+  public static void main(args[] string){
+    System.out.print("Hello World");
+    System.out.print("codecutr was here!");
+  }
 }
