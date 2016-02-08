5cefb56a1ad24b59d839fa0613be9f06c70d9463
Zach <z-ch@users.noreply.github.com>
2016-02-08 17:40:07 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 // Mon Feb  8 17:34:21 EST 2016
 // Zach, Cat, Alyssa, Tim, Sean
 public class HelloWorld {
-  public static void main(args[] string){
+  public static void main(String args[]){
     System.out.print("Hello World");
     System.out.print("codecutr was here!");
   }
